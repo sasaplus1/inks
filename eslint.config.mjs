f52fffbb74d199f3b887ec1fb7b6e6a890c95904
sasaplus1 <sasaplus1@gmail.com>
2025-03-28 18:18:24 +0900

    chore: temp

---
+++ eslint.config.mjs
@@ -0,0 +1,102 @@
+import { defineConfig } from 'eslint/config';
+import tsdoc from 'eslint-plugin-tsdoc';
+
+const typescriptExtends = [
+  'eslint:recommended',
+  'plugin:@typescript-eslint/eslint-recommended',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:react/recommended',
+  'plugin:react-hooks/recommended',
+  'prettier'
+];
+
+const typescriptPlugins = {
+  tsdoc
+};
+
+// const tsdocRules = {
+//   'tsdoc/syntax': 'warn'
+// };
+
+const reactPluginSettings = {
+  react: {
+    version: 'detect'
+  }
+};
+
+// const config = {};
+// const overrides = [];
+//
+// overrides.push({
+//   extends: typescriptExtends,
+//   files: ['./**/!(*.test|*.spec).{ts,tsx}'],
+//   plugins: [...typescriptPlugins],
+//   rules: {
+//     ...tsdocRules
+//   },
+//   settings: {
+//     ...reactPluginSettings
+//   }
+// });
+//
+// overrides.push({
+//   extends: typescriptExtends,
+//   files: ['./**/*.+(test|spec).{ts,tsx}'],
+//   plugins: [...typescriptPlugins],
+//   rules: {
+//     ...tsdocRules
+//   },
+//   settings: {
+//     ...reactPluginSettings
+//   }
+// });
+
+/*
+config.env = {
+  es6: true,
+  node: true
+};
+*/
+// config.extends = ['eslint:recommended', 'prettier'];
+// config.overrides = overrides;
+// config.parserOptions = {
+//   ecmaVersion: 'latest',
+//   sourceType: 'module'
+// };
+// config.root = true;
+
+export default defineConfig([
+  {
+    extends: ['eslint:recommended', 'prettier'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    }
+  },
+  {
+    extends: typescriptExtends,
+    // NOTE: for sources
+    files: ['./**/!(*.test|*.spec).{ts,tsx}'],
+    plugins: { ...typescriptPlugins },
+    rules: {
+      'tsdoc/syntax': 'warn'
+    },
+    settings: {
+      ...reactPluginSettings
+    }
+  },
+  {
+    extends: typescriptExtends,
+    // NOTE: for tests
+    files: ['./**/*.+(test|spec).{ts,tsx}'],
+    plugins: { ...typescriptPlugins },
+    rules: {
+      'tsdoc/syntax': 'warn'
+    },
+    settings: {
+      ...reactPluginSettings
+    }
+  }
+]);
